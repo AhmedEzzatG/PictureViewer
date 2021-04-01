8a2c63881fdbd9bcacdba6ecc6743ddb767f474f
Ahmed Ezzat <ahmed,ezzat.ga@gmail.com>
2021-04-01 19:38:24 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.add_images = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.imagesPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.currentFileSatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.singlePictureModeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.multiPictureModeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.slideshowModeMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.currentModeSatus = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.imagesPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(30, 30);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.HideSelection = false;
+            this.listView1.LargeImageList = this.imageList1;
+            this.listView1.Location = new System.Drawing.Point(865, 3);
+            this.listView1.MinimumSize = new System.Drawing.Size(30, 30);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(211, 562);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.show_items);
+            // 
+            // add_images
+            // 
+            this.add_images.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.add_images.Location = new System.Drawing.Point(3, 604);
+            this.add_images.Name = "add_images";
+            this.add_images.Size = new System.Drawing.Size(425, 62);
+            this.add_images.TabIndex = 0;
+            this.add_images.Text = "upload pictures";
+            this.add_images.UseVisualStyleBackColor = true;
+            this.add_images.Click += new System.EventHandler(this.upload_images);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.clearButton.Location = new System.Drawing.Point(434, 604);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(425, 62);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.currentModeSatus, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.clearButton, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.listView1, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.imagesPanel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.statusStrip1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.add_images, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 85F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1079, 669);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // imagesPanel
+            // 
+            this.imagesPanel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.tableLayoutPanel1.SetColumnSpan(this.imagesPanel, 2);
+            this.imagesPanel.Controls.Add(this.pictureBox1);
+            this.imagesPanel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imagesPanel.Location = new System.Drawing.Point(3, 3);
+            this.imagesPanel.Name = "imagesPanel";
+            this.imagesPanel.Size = new System.Drawing.Size(856, 562);
+            this.imagesPanel.TabIndex = 4;
+            this.imagesPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            // 
+            // statusStrip1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.statusStrip1, 3);
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.currentFileSatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 568);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1079, 33);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // currentFileSatus
+            // 
+            this.currentFileSatus.Name = "currentFileSatus";
+            this.currentFileSatus.Size = new System.Drawing.Size(0, 26);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.singlePictureModeMenuItem,
+            this.multiPictureModeMenuItem,
+            this.slideshowModeMenuItem,
+            this.stopToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStrip1.ShowCheckMargin = true;
+            this.contextMenuStrip1.ShowImageMargin = false;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(190, 164);
+            // 
+            // singlePictureModeMenuItem
+            // 
+            this.singlePictureModeMenuItem.Name = "singlePictureModeMenuItem";
+            this.singlePictureModeMenuItem.Size = new System.Drawing.Size(189, 32);
+            this.singlePictureModeMenuItem.Text = "single picture";
+            this.singlePictureModeMenuItem.Click += new System.EventHandler(this.show_items);
+            // 
+            // multiPictureModeMenuItem
+            // 
+            this.multiPictureModeMenuItem.Name = "multiPictureModeMenuItem";
+            this.multiPictureModeMenuItem.Size = new System.Drawing.Size(189, 32);
+            this.multiPictureModeMenuItem.Text = "multi-picture";
+            this.multiPictureModeMenuItem.Click += new System.EventHandler(this.show_items);
+            // 
+            // slideshowModeMenuItem
+            // 
+            this.slideshowModeMenuItem.Name = "slideshowModeMenuItem";
+            this.slideshowModeMenuItem.Size = new System.Drawing.Size(189, 32);
+            this.slideshowModeMenuItem.Text = "Slide Show";
+            this.slideshowModeMenuItem.Click += new System.EventHandler(this.slideshowMode);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(189, 32);
+            this.stopToolStripMenuItem.Text = "stop";
+            this.stopToolStripMenuItem.Visible = false;
+            this.stopToolStripMenuItem.Click += new System.EventHandler(this.stopToolStripMenuItem_Click);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(189, 32);
+            this.closeToolStripMenuItem.Text = "close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // currentModeSatus
+            // 
+            this.currentModeSatus.AutoSize = true;
+            this.currentModeSatus.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.currentModeSatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.currentModeSatus.Location = new System.Drawing.Point(865, 601);
+            this.currentModeSatus.Name = "currentModeSatus";
+            this.currentModeSatus.Size = new System.Drawing.Size(211, 68);
+            this.currentModeSatus.TabIndex = 7;
+            this.currentModeSatus.Text = "Current Mode is : standby";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1079, 669);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Picture Viewer";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.imagesPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button add_images;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.FlowLayoutPanel imagesPanel;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel currentFileSatus;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem singlePictureModeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem multiPictureModeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem slideshowModeMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label currentModeSatus;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        List<string> files = new List<string>();
+        int currentImage = 0;
+        int currentMode = 3;
+        private void upload_images(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "select images";
+            file.Multiselect = true;
+            file.ValidateNames = true;
+            file.Filter = "Image Files(*.png;*.JPG)|*.png;*.JPG";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                foreach (string image in file.FileNames)
+                {
+                    FileInfo fi = new FileInfo(image); 
+                    string path =  fi.FullName;
+                    if (files.Contains(path)) continue;
+                    imageList1.Images.Add(image, new Bitmap(path));
+                    ListViewItem item = new ListViewItem();
+                    item.ImageIndex = files.Count;
+                    item.Text = fi.Name;
+                    listView1.Items.Add(item);
+                    files.Add(path);
+                }
+            }
+        }
+
+        private PictureBox get_image(int index)
+        {
+            PictureBox p1 = new PictureBox();
+            p1.SizeMode = PictureBoxSizeMode.Zoom;
+            p1.Image = Image.FromFile(files[index]);
+            p1.MouseClick += Form1_MouseClick;
+            return p1;
+        }
+        private void show_items(object sender, EventArgs e)
+        {
+            if (currentMode == 3)
+            {
+                if (sender == singlePictureModeMenuItem)
+                {
+                    currentMode = 0;
+                }
+                else if (sender == multiPictureModeMenuItem)
+                {
+                    currentMode = 1;
+                }
+                else return;
+                changeMenuItems();
+            }
+            if (currentMode == 2)
+                return;
+            imagesPanel.Controls.Clear();
+            foreach (ListViewItem item in listView1.SelectedItems)
+            {
+                imagesPanel.Controls.Add(get_image(item.ImageIndex));
+            }
+            int cnt = imagesPanel.Controls.Count;
+            if (cnt == 0)
+            {
+                return;
+            }
+            if (currentMode == 0)
+            {
+                cnt = 1;
+            }
+            int height = imagesPanel.Size.Height;
+            int width = imagesPanel.Size.Width;
+            height -= 10;
+            width -= 30;
+            int x = 1;
+            while (x * x < cnt) x++;
+            height = Math.Max(0, height / x);
+            width = Math.Max(0, width / x);
+            for (int i = 0; i < cnt; i++)
+            {
+                imagesPanel.Controls[i].Size = new Size(width, height);
+            }
+        }
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            imageList1.Images.Clear();
+            files.Clear();
+            imagesPanel.Controls.Clear();
+        }
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(new Point(e.X, e.Y));
+            }
+        }
+
+        private void slideshowMode(object sender, EventArgs e)
+        {
+            if (files.Count == 0)
+            {
+                MessageBox.Show("Please upload at Least 1 picture");
+                return;
+            }
+            currentMode = 2;
+            changeMenuItems();
+            currentImage = 0;
+            timer1.Stop();
+            timer1.Interval = 1000;
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (currentImage >= files.Count)
+            {
+                stopToolStripMenuItem_Click(sender, e);
+                return;
+            }
+            pictureBox1 = get_image(currentImage);
+            pictureBox1.Size = imagesPanel.Size;
+            imagesPanel.Controls.Clear();
+            imagesPanel.Controls.Add(pictureBox1);
+            currentFileSatus.Text = files[currentImage];
+            currentImage++;
+        }
+
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            currentFileSatus.Text = "";
+            currentMode = 3;
+            changeMenuItems();
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void changeMenuItems()
+        {
+            listView1.MultiSelect = (currentMode != 0);
+            singlePictureModeMenuItem.Visible = (currentMode == 3);
+            multiPictureModeMenuItem.Visible = (currentMode == 3);
+            slideshowModeMenuItem.Visible = (currentMode == 3);
+            stopToolStripMenuItem.Visible = (currentMode != 3);
+            currentModeSatus.Text = "Current Mode is : ";
+            switch (currentMode)
+            {
+                case 0:
+                    currentModeSatus.Text += "single Picture";
+                    break;
+                case 1:
+                    currentModeSatus.Text += "multi-Pictures";
+                    break;
+                case 2:
+                    currentModeSatus.Text += "slide show";
+                    break;
+                default:
+                    currentModeSatus.Text += "standby";
+                    break;
+            }
+        }
+    }
+}
